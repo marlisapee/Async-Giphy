fcf84c8a79b12b2e5891e71b6a97dd98486e0f0e
marlisapee <mpee@tripadvisor.com>
2024-04-29 19:57:07 -0400

    added random num to function

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 
-const getImage = async (query) => {
-  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=1&offset=0&rating=g&lang=en`;
+const randomNum = Math.floor(Math.random() * 25 + 1);
+
+const getImage = async (query, num) => {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
   const response = await fetch(endpoint);
   const data = await response.json();
-  const oneGifUrl = data.data[0].url;
-  console.log(oneGifUrl);
+  const randomGif = data.data[num];
+  console.log(randomGif);
+  return randomGif;
 };
 
-getImage('dogs');
+getImage('dogs', randomNum);
