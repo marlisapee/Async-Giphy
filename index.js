18b7d33278329b5e87f2aa94fe441d9cd3580625
marlisapee <mpee@tripadvisor.com>
2024-04-29 19:46:27 -0400

    dynamic query in endpoint

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 require('dotenv').config();
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+const getImage = async (query) => {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=1&offset=0&rating=g&lang=en`;
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  const oneGifUrl = data.data[0].url;
+  console.log(oneGifUrl);
+};
+
+getImage('dogs');
