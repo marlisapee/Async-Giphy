1d585506ee64f1d44be2376eb06fd107b657c416
marlisapee <mpee@tripadvisor.com>
2024-04-29 20:00:53 -0400

    create data array with only gif urls

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const getImage = async (query, num) => {
   const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
   const response = await fetch(endpoint);
   const data = await response.json();
+
   const randomGif = data.data[num];
   console.log(randomGif);
-  return randomGif;
+
+  const dataArr = [];
+  data.data.map((gif) => dataArr.push(gif.url));
+  console.log(dataArr);
+  return dataArr;
 };
 
 getImage('dogs', randomNum);
